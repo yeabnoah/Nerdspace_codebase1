d31e04399718c0ca35875e08120d851fce841e43
yeabnoah <yeabnoah5@gmail.com>
2025-03-10 20:32:04 +0300

    migration : from zustand to react-query

---
+++ src/hooks/useFetchPosts.tsx
@@ -0,0 +1,15 @@
+// import { useQuery } from "@tanstack/react-query";
+// import axios from "axios";
+// import postInterface from "@/interface/auth/post.interface";
+
+// const useFetchPosts = () => {
+//   return useQuery({
+//     queryKey: ["posts"],
+//     queryFn: async () => {
+//       const response = await axios.get("/api/post");
+//       return response.data.data as postInterface[];
+//     },
+//   });
+// };
+
+// export default useFetchPosts;
--- src/providers/tanstack-query-provider.tsx
+++ src/providers/tanstack-query-provider.tsx
@@ -3,9 +3,10 @@
 import { QueryClient, QueryClientProvider  } from "@tanstack/react-query"
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 import { ReactNode } from 'react'
+export const queryClient = new QueryClient()
 
 const TanstackQueryProvider = ({children} : {children : ReactNode}) => {
-    const queryClient = new QueryClient()
+    
   return (
     <QueryClientProvider client={queryClient}>
         {children}
