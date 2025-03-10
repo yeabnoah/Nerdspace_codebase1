ba0147c03211f6fbab3280c8d4e09152f5a17a1c
yeabnoah <yeabnoah5@gmail.com>
2025-03-10 12:16:05 +0300

    coding the layout for the rendered post

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["avatars.githubusercontent.com"], // Allow GitHub Avatars
+  },
   experimental: {
     staleTimes: {
       dynamic: 30,
