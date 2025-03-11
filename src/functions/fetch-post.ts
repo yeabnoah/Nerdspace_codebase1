6facc9fb93a564a1338a48b1d01357c8ad2d4a7a
yeabnoah <yeabnoah5@gmail.com>
2025-03-11 11:07:12 +0300

    auto loading and pagination working

--- src/functions/fetch-post.ts
+++ src/functions/fetch-post.ts
@@ -1,9 +1,11 @@
 import postInterface from "@/interface/auth/post.interface";
 import axios from "axios";
 
-const fetchPosts = async () => {
-  const response = await axios.get("/api/post");
-  return response.data.data as postInterface[];
+const fetchPosts = async ({ pageParam }: { pageParam: string | null }) => {
+  const response = await axios.get("/api/post", {
+    params: { cursor: pageParam },
+  });
+  return response.data;
 };
 
 export default fetchPosts;
