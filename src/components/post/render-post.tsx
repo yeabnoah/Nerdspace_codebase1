49c2665ed8da2eb58c49d0c4885f168cc1bca58c
yeabnoah <yeabnoah5@gmail.com>
2025-03-11 11:53:44 +0300

    skeleton done

--- src/components/post/render-post.tsx
+++ src/components/post/render-post.tsx
@@ -9,9 +9,11 @@ import { BookmarkIcon, Heart, MessageCircle } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import { Skeleton } from "../ui/skeleton";
+import { Card } from "../ui/card";
 
 const RenderPost = () => {
-  const { ref, inView  } = useInView();
+  const { ref, inView } = useInView();
 
   const {
     data,
@@ -57,8 +59,52 @@ const RenderPost = () => {
 
   if (isLoading) {
     return (
-      <div className="flex h-[60vh] w-full items-center justify-center">
-        loading ..
+      <div>
+        <Card className="my-5 rounded-xl border bg-transparent p-4">
+          <div className="flex items-center gap-5">
+            <Skeleton className="size-10 rounded-full" />
+            <div>
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="mt-1 h-3 w-16" />
+            </div>
+          </div>
+          <Skeleton className="mt-4 h-16 w-full" />
+          <div className="mt-4 flex gap-3">
+            <Skeleton className="h-6 w-6 rounded-full" />
+            <Skeleton className="h-6 w-6 rounded-full" />
+            <Skeleton className="h-6 w-6 rounded-full" />
+          </div>
+        </Card>
+        <Card className="my-5 rounded-xl border bg-transparent p-4">
+          <div className="flex items-center gap-5">
+            <Skeleton className="size-10 rounded-full" />
+            <div>
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="mt-1 h-3 w-16" />
+            </div>
+          </div>
+          <Skeleton className="mt-4 h-16 w-full" />
+          <div className="mt-4 flex gap-3">
+            <Skeleton className="h-6 w-6 rounded-full" />
+            <Skeleton className="h-6 w-6 rounded-full" />
+            <Skeleton className="h-6 w-6 rounded-full" />
+          </div>
+        </Card>
+        <Card className="my-5 rounded-xl border bg-transparent p-4">
+          <div className="flex items-center gap-5">
+            <Skeleton className="size-10 rounded-full" />
+            <div>
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="mt-1 h-3 w-16" />
+            </div>
+          </div>
+          <Skeleton className="mt-4 h-16 w-full" />
+          <div className="mt-4 flex gap-3">
+            <Skeleton className="h-6 w-6 rounded-full" />
+            <Skeleton className="h-6 w-6 rounded-full" />
+            <Skeleton className="h-6 w-6 rounded-full" />
+          </div>
+        </Card>
       </div>
     );
   }
@@ -83,9 +129,7 @@ const RenderPost = () => {
           const isShortContent = contentWords.length < trimLimit;
           const isTooShort = contentWords.length < 10;
 
-          inView && hasNextPage && (
-            fetchNextPage()
-          )
+          inView && hasNextPage && fetchNextPage();
 
           return (
             <div className="my-5 rounded-xl border p-4" key={index}>
@@ -148,9 +192,38 @@ const RenderPost = () => {
         })}
       <div ref={ref}>
         {isFetchingNextPage && (
-          <div className="flex items-center justify-center">
-            <p>Loading more posts...</p>
-            </div>
+          <div className="">
+            <Card className="my-5 rounded-xl border bg-transparent p-4">
+              <div className="flex items-center gap-5">
+                <Skeleton className="size-10 rounded-full" />
+                <div>
+                  <Skeleton className="h-4 w-24" />
+                  <Skeleton className="mt-1 h-3 w-16" />
+                </div>
+              </div>
+              <Skeleton className="mt-4 h-16 w-full" />
+              <div className="mt-4 flex gap-3">
+                <Skeleton className="h-6 w-6 rounded-full" />
+                <Skeleton className="h-6 w-6 rounded-full" />
+                <Skeleton className="h-6 w-6 rounded-full" />
+              </div>
+            </Card>
+            {/* <Card className="my-5 rounded-xl border bg-transparent p-4">
+              <div className="flex items-center gap-5">
+                <Skeleton className="size-10 rounded-full" />
+                <div>
+                  <Skeleton className="h-4 w-24" />
+                  <Skeleton className="mt-1 h-3 w-16" />
+                </div>
+              </div>
+              <Skeleton className="mt-4 h-16 w-full" />
+              <div className="mt-4 flex gap-3">
+                <Skeleton className="h-6 w-6 rounded-full" />
+                <Skeleton className="h-6 w-6 rounded-full" />
+                <Skeleton className="h-6 w-6 rounded-full" />
+              </div>
+            </Card> */}
+          </div>
         )}
       </div>
     </div>
