6737e65035430466296677bca165bcf81355a3db
yeabnoah <yeabnoah5@gmail.com>
2025-03-12 18:11:16 +0300

    zustand : selected post track added

--- src/store/post.store.ts
+++ src/store/post.store.ts
@@ -4,10 +4,30 @@ import { create } from "zustand";
 
 interface PostStoreInterface {
   posts: postInterface[];
+  selectedPost: postInterface;
+  setSelectedPost: (post: postInterface) => void;
 }
 
 const usePostStore = create<PostStoreInterface>((set, get) => ({
   posts: [],
+  selectedPost: {
+    id: "",
+    content: "",
+    createdAt: null,
+    updatedAt: null,
+    userId: "",
+    user: {
+      id: "",
+      name: "",
+      email: "",
+      emailVerified: false,
+      image: "",
+      createdAt: null,
+      updatedAt: null,
+      nerdAt: null,
+    },
+  },
+  setSelectedPost: (post: postInterface) => set({ selectedPost: post }),
 }));
 
 export default usePostStore;
