c30275f9ad9ec2cdb943d29830f53c8e23033649
yeabnoah <yeabnoah5@gmail.com>
2025-03-15 17:29:13 +0300

    working : api reply working .. geting comments working

--- src/app/api/post/comment/route.ts
+++ src/app/api/post/comment/route.ts
@@ -1,13 +1,7 @@
 import getUserSession from "@/functions/get-user";
 import { prisma } from "@/lib/prisma";
+import commentSchema from "@/validation/comment.validation";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
-
-const commentSchema = z.object({
-  postId: z.string(),
-  content: z.string(),
-  parentId: z.string().optional(),
-});
 
 export const POST = async (req: NextRequest) => {
   try {
@@ -48,6 +42,51 @@ export const POST = async (req: NextRequest) => {
     return NextResponse.json(
       {
         data: newComment,
+      },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.error("Error creating comment:", err);
+    return NextResponse.json({ error: "error" }, { status: 500 });
+  }
+};
+
+export const GET = async (request: NextRequest) => {
+  try {
+    const session = await getUserSession();
+    const postId = await request.nextUrl.searchParams.get("postId");
+
+    if (!postId) {
+      return NextResponse.json(
+        {
+          message: "postId query parameter is required",
+        },
+        { status: 400 },
+      );
+    }
+
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "unauthorized | not logged in",
+        },
+        { status: 400 },
+      );
+    }
+
+    const allComments = await prisma.postComment.findMany({
+      where: {
+        postId: postId,
+      },
+      include: {
+        replies: true,
+        parent: true,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        data: allComments,
       },
       { status: 200 },
     );
