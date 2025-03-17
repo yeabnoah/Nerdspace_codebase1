7d5eee534a0d8d3cb8bc022f1eb259a2ff258725
yeabnoah <yeabnoah5@gmail.com>
2025-03-17 15:47:53 +0300

    nested comment implimented

--- src/components/post/render-post.tsx
+++ src/components/post/render-post.tsx
@@ -22,6 +22,10 @@ import {
   SendIcon,
   Share2Icon,
   TrashIcon,
+  ChevronDown,
+  ChevronRight,
+  Dot,
+  MessageCircleIcon,
 } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
@@ -41,7 +45,7 @@ import toast from "react-hot-toast";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import axios from "axios";
-import { Value } from "@radix-ui/react-select";
+import PostCommentInterface from "@/interface/auth/comment.interface";
 
 const RenderPost = () => {
   const { ref, inView } = useInView();
@@ -51,7 +55,39 @@ const RenderPost = () => {
   const [editPostInput, setEditPostInput] = useState<String>();
   const [deleteModal, setDeleteModal] = useState<boolean>(false);
   const [commentShown, setCommentShown] = useState<boolean>(false);
-  const [commentContent, setCommentContent] = useState<String>();
+  const [expandedComments, setExpandedComments] = useState<{
+    [key: string]: boolean;
+  }>({});
+  const [replyShown, setReplyShown] = useState<{ [key: string]: boolean }>({});
+  const [replyContent, setReplyContent] = useState<{ [key: string]: string }>(
+    {},
+  );
+  const [commentContent, setCommentContent] = useState<string>("");
+
+  const toggleCommentExpand = (commentId: string) => {
+    setExpandedComments((prev) => ({
+      ...prev,
+      [commentId]: !prev[commentId],
+    }));
+  };
+
+  const toggleReplyShown = (commentId: string) => {
+    setReplyShown((prev) => ({
+      ...prev,
+      [commentId]: !prev[commentId],
+    }));
+  };
+
+  const [expandedReplies, setExpandedReplies] = useState<{
+    [key: string]: boolean;
+  }>({});
+
+  const toggleReplies = (commentId: string) => {
+    setExpandedReplies((prev) => ({
+      ...prev,
+      [commentId]: !prev[commentId],
+    }));
+  };
 
   const {
     data,
@@ -67,31 +103,17 @@ const RenderPost = () => {
     getNextPageParam: (lastPage) => lastPage.nextCursor,
   });
 
-  const { data: comment } = useQuery({
+  const { data: comment, isLoading: commentLoading } = useQuery({
     queryKey: ["comment"],
     queryFn: async () => {
       const response = await axios.get(
-        `/api/post/comment?postId=${selectedPost.id}}`,
+        `/api/post/comment?postId=${selectedPost.id}`,
       );
+
+      console.log(`/api/post/comment?postId=${selectedPost.id}`);
+
       console.log("test", response.data.data);
       return response.data.data;
-    },
-  });
-
-  const commentMutation = useMutation({
-    mutationKey: ["post-comment"],
-    mutationFn: async () => {
-      const response = await axios.post("/api/post/comment", {
-        postId: selectedPost.id,
-        content: commentContent,
-      });
-      return response.data.data;
-    },
-    onSuccess: () => {
-      toast.success("commented successfully");
-    },
-    onError: () => {
-      toast.error("Error happened while commenting");
     },
   });
 
@@ -106,6 +128,70 @@ const RenderPost = () => {
     },
   });
 
+  const replyMutation = useMutation({
+    mutationKey: ["reply-comment"],
+    mutationFn: async ({
+      commentId,
+      content,
+    }: {
+      commentId: string;
+      content: string;
+    }) => {
+      const response = await axios.post("/api/post/comment", {
+        postId: selectedPost.id,
+        content,
+        parentId: commentId,
+      });
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comment"] });
+    },
+    onError: () => {
+      toast.error("Error occurred while replying to comment");
+    },
+  });
+
+  const commentMutation = useMutation({
+    mutationKey: ["add-comment"],
+    mutationFn: async ({
+      postId,
+      content,
+    }: {
+      postId: string;
+      content: string;
+    }) => {
+      const response = await axios.post("/api/post/comment", {
+        postId,
+        content,
+      });
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comment"] });
+      setCommentContent("");
+    },
+    onError: () => {
+      toast.error("Error occurred while adding comment");
+    },
+  });
+
+  const handleReplySubmit = (commentId: string) => {
+    if (replyContent[commentId]) {
+      replyMutation.mutate({ commentId, content: replyContent[commentId] });
+      setReplyContent((prev) => ({ ...prev, [commentId]: "" }));
+    }
+  };
+
+  const handleCommentSubmit = () => {
+    if (commentContent) {
+      commentMutation.mutate({
+        postId: selectedPost.id,
+        content: commentContent,
+      });
+    }
+  };
+
   const [expandedStates, setExpandedStates] = useState<boolean[]>(
     Array(data?.pages?.flatMap((page) => page.data)?.length).fill(false),
   );
@@ -116,6 +202,114 @@ const RenderPost = () => {
       newExpandedStates[index] = !newExpandedStates[index];
       return newExpandedStates;
     });
+  };
+
+  const renderComments = (
+    comments: PostCommentInterface[],
+    parentId: String | null,
+    level = 0,
+  ) => {
+    return (
+      comments &&
+      comments
+        .filter(
+          (comment: PostCommentInterface) => comment?.parentId === parentId,
+        )
+        .map((comment) => {
+          const contentWords = comment.content.split(" ");
+          const trimLimit = 20; // Adjust the trim limit as needed
+          const truncatedContent = contentWords.slice(0, trimLimit).join(" ");
+          const isLongContent = contentWords.length > trimLimit;
+
+          return (
+            <div
+              key={comment.id}
+              className={`relative my-1 py-2 ml-${level * 4} pl-4`}
+            >
+              <div className="absolute left-0 top-0 ml-4 h-full w-6 rounded-l border-b border-l border-t-0 border-white/5"></div>
+              <div className="flex items-center justify-between pl-2">
+                <div className="flex gap-2">
+                  <Image
+                    className="size-8 rounded-full"
+                    src={comment.user?.image || "/user.jpg"}
+                    height={200}
+                    width={200}
+                    alt="user"
+                  />
+                  <div className="flex items-center">
+                    <h4 className="text-xs">{comment?.user?.visualName}</h4>
+                    <Dot />
+                    <p className="text-xs">{timeAgo(comment.createdAt)}</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-2">
+                  <button
+                    className="rounded-xl border p-2"
+                    onClick={() => {
+                      toggleReplyShown(comment.id);
+                    }}
+                  >
+                    <MessageCircleIcon size={16} className="" />
+                  </button>
+                  <button
+                    className="rounded-xl border p-2"
+                    onClick={() => toggleReplies(comment.id)}
+                  >
+                    {expandedReplies[comment.id] ? (
+                      <ChevronDown size={16} />
+                    ) : (
+                      <ChevronRight size={16} />
+                    )}
+                  </button>
+                </div>
+              </div>
+
+              <div className="mt-2 w-[90%] pl-4">
+                <p className="text-xs">
+                  {expandedComments[comment.id] || !isLongContent
+                    ? comment.content
+                    : `${truncatedContent}...`}
+                </p>
+                {isLongContent && (
+                  <button
+                    className="mt-2 text-xs underline"
+                    onClick={() => toggleCommentExpand(comment.id)}
+                  >
+                    {expandedComments[comment.id] ? "See less" : "See more"}
+                  </button>
+                )}
+                {expandedComments[comment.id] &&
+                  renderComments(comments, comment?.id, level + 1)}
+              </div>
+
+              {replyShown[comment.id] && (
+                <div className="mt-2 flex items-start gap-2 pl-8">
+                  <input
+                    placeholder="Reply here"
+                    className="w-full border-0 border-b border-white/5 bg-transparent p-1 text-sm placeholder:font-instrument placeholder:text-lg focus:border-b focus:border-gray-500 focus:outline-none focus:ring-0"
+                    value={replyContent[comment.id] || ""}
+                    onChange={(e) =>
+                      setReplyContent((prev) => ({
+                        ...prev,
+                        [comment.id]: e.target.value,
+                      }))
+                    }
+                  />
+                  <Button
+                    className="mt-2 rounded-lg border bg-transparent px-2 py-1 hover:bg-transparent focus:outline-none focus:ring-0"
+                    onClick={() => handleReplySubmit(comment.id)}
+                  >
+                    <SendIcon color="white" size={8} />
+                  </Button>
+                </div>
+              )}
+
+              {expandedReplies[comment.id] &&
+                renderComments(comments, comment.id, level + 1)}
+            </div>
+          );
+        })
+    );
   };
 
   if (isLoading) {
@@ -252,7 +446,6 @@ const RenderPost = () => {
                       ? each.content
                       : `${truncatedContent}...`}
                   </h4>
-                  //tedt
                   {isLongContent && (
                     <button
                       className="mt-2 text-xs underline"
@@ -272,9 +465,12 @@ const RenderPost = () => {
                     <Heart className="size-5" />
                   </div>
                   <div
-                    onClick={() => {
-                      setSelectedPost(each);
+                    onClick={async () => {
+                      await setSelectedPost(each);
                       setCommentShown(!commentShown);
+                      await queryClient.invalidateQueries({
+                        queryKey: ["comment"],
+                      });
                     }}
                     className={`cursor-pointer rounded-full ${isShortContent && isTooShort ? "pr-2" : "px-2"}`}
                   >
@@ -293,23 +489,53 @@ const RenderPost = () => {
                   <hr className="mb-2 mt-5" />
                   <div className="itemc flex gap-2">
                     <input
-                      onChange={(e) => {
-                        setCommentContent(e.target.value);
-                      }}
                       placeholder="Comment here"
                       className="w-full border-0 border-b border-white/50 bg-transparent text-sm placeholder:font-instrument placeholder:text-lg focus:border-b focus:border-gray-500 focus:outline-none focus:ring-0"
+                      value={commentContent}
+                      onChange={(e) => setCommentContent(e.target.value)}
                     />
                     <Button
-                      onClick={async () => {
-                        await commentMutation.mutate();
-                      }}
-                      className="border bg-transparent transition-all hover:scale-105 hover:bg-transparent focus:outline-none focus:ring-0"
+                      onClick={handleCommentSubmit}
+                      className="border bg-transparent hover:bg-transparent focus:outline-none focus:ring-0"
                     >
                       <SendIcon color="white" />
                     </Button>
                   </div>
-
-                  <div></div>
+                  {commentLoading && (
+                    <div>
+                      <Skeleton className="my-2 ml-4 h-fit border bg-black/5 pb-3 pl-4">
+                        <div className="flex items-center gap-3">
+                          <Skeleton className="mt-2 size-10 rounded-full" />
+                          <div className="flex flex-1 flex-col gap-2">
+                            <div className="flex flex-1 items-center gap-2">
+                              <Skeleton className="h-3 w-3/4 rounded" />
+                            </div>
+                            <div className="flex flex-1 items-center gap-2">
+                              <Skeleton className="h-3 w-1/3 rounded" />
+                            </div>
+                          </div>
+                          {/* <div></div> */}
+                        </div>
+                        <Skeleton className="my-2 h-4 w-[95%] rounded" />
+                      </Skeleton>
+                      <Skeleton className="my-2 ml-4 h-fit border bg-black/5 pb-3 pl-4">
+                        <div className="flex items-center gap-3">
+                          <Skeleton className="mt-2 size-10 rounded-full" />
+                          <div className="flex flex-1 flex-col gap-2">
+                            <div className="flex flex-1 items-center gap-2">
+                              <Skeleton className="h-3 w-3/4 rounded" />
+                            </div>
+                            <div className="flex flex-1 items-center gap-2">
+                              <Skeleton className="h-3 w-1/3 rounded" />
+                            </div>
+                          </div>
+                          {/* <div></div> */}
+                        </div>
+                        <Skeleton className="my-2 h-4 w-[95%] rounded" />
+                      </Skeleton>
+                    </div>
+                  )}
+                  <div className="mt-4">{renderComments(comment, null)}</div>
                 </div>
               )}
             </div>
