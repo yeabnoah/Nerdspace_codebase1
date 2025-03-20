3e2635444ab2dab7fddf93e5ec45fb53f9fa33c6
yeabnoah <yeabnoah5@gmail.com>
2025-03-20 10:50:13 +0300

    api for media upload and track

---
+++ src/app/api/media-upload/route.ts
@@ -0,0 +1,52 @@
+import getUserSession from "@/functions/get-user";
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const MediaSchema = z.object({
+  postId: z.string(),
+  url: z.string().url(),
+  mediaType: z.enum(["IMAGE", "VIDEO", "GIF"]),
+});
+
+type MediaType = z.infer<typeof MediaSchema>;
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const session = await getUserSession();
+    const body: MediaType = await req.json();
+
+    if (!session) {
+      return NextResponse.json(
+        { message: "unauthorized | not logged in" },
+        { status: 400 },
+      );
+    }
+
+    const validationValue = MediaSchema.safeParse(body);
+    if (!validationValue.success || validationValue.error) {
+      return NextResponse.json(
+        { message: "validation error", error: validationValue.error.message },
+        { status: 401 },
+      );
+    }
+
+    const newMedia = await prisma.media.create({
+      data: {
+        postId: body.postId,
+        url: body.url,
+        type: body.mediaType,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        data: newMedia,
+        message: "Media uploaded successfully",
+      },
+      { status: 201 },
+    );
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+};
