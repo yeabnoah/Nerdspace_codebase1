8b14a383f96b72f4753c3fee854ac9a1c9d5aa1e
yeabnoah <yeabnoah5@gmail.com>
2025-03-25 17:49:25 +0300

    working who to follow

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -55,15 +55,17 @@ const UserList: React.FC<UserListProps> = ({ handleFollow }) => {
   const nextCursor: string | null = data?.nextCursor || null;
 
   return (
-    <div className="container mx-auto my-5 max-w-4xl">
-      <h1 className="mb-5 font-instrument text-3xl">Who to Follow</h1>
-      <div className="rounded-lg bg-white p-5">
+    <div className="container mx-auto my-5 max-w-4xl px-4 sm:px-6 lg:px-8">
+      <h1 className="mb-5 text-center font-instrument text-3xl sm:text-left">
+        Who to Follow
+      </h1>
+      <div className="rounded-lg">
         <div className="space-y-4">
           {users.length > 0 ? (
             users.map((u) => (
               <div
                 key={u.id}
-                className="flex items-center justify-between border-b border-gray-200 p-4"
+                className="flex flex-col items-center justify-between py-1 sm:flex-row"
               >
                 <div className="flex items-center gap-4">
                   <Image
@@ -73,7 +75,7 @@ const UserList: React.FC<UserListProps> = ({ handleFollow }) => {
                     height={200}
                     width={200}
                   />
-                  <div className="flex flex-col">
+                  <div className="flex flex-col text-center sm:text-left">
                     <span className="font-semibold">{u.visualName}</span>
                     <span className="text-sm text-gray-500">
                       Nerd@{u.nerdAt}
@@ -81,8 +83,8 @@ const UserList: React.FC<UserListProps> = ({ handleFollow }) => {
                   </div>
                 </div>
                 <Button
-                  size="sm"
-                  className="border bg-transparent text-blue-500 shadow-none hover:bg-blue-50"
+                  variant={"outlined"}
+                  className="mt-2 border bg-transparent shadow-none sm:mt-0"
                   onClick={() => handleFollowClick(u.id)}
                 >
                   Follow
@@ -90,7 +92,9 @@ const UserList: React.FC<UserListProps> = ({ handleFollow }) => {
               </div>
             ))
           ) : (
-            <p className="text-sm text-gray-500">No suggestions available</p>
+            <p className="text-center text-sm text-gray-500">
+              No suggestions available
+            </p>
           )}
         </div>
         {nextCursor && (
