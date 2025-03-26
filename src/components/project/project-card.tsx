239afa19826656e5bfcdec34ff1d0af08f67ecbf
yeabnoah <yeabnoah5@gmail.com>
2025-03-26 19:30:57 +0300

    working on the ui change for the project-card

--- src/components/project/project-card.tsx
+++ src/components/project/project-card.tsx
@@ -3,34 +3,26 @@ import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import ProjectInterface from "@/interface/auth/project.interface";
+import { StarHalf } from "lucide-react";
+import { GoStarFill } from "react-icons/go";
 
 export default function ProjectCard(project: ProjectInterface) {
   const primaryCategory = project.category[0] || "Project";
 
   return (
-    <Card className="my-2 flex flex-col overflow-hidden rounded-2xl border shadow-none">
+    <Card className="my-2 flex flex-col overflow-hidden rounded-2xl border shadow-lg">
       <CardContent
         className="relative flex h-64 flex-col justify-end bg-cover p-4"
         style={{
           backgroundImage: `url(${project.image || "/placeholder.svg"})`,
         }}
       >
-        <div
-          className="absolute inset-0 h-full bg-gradient-to-t from-black via-black/50 to-transparent"
-          style={{ filter: "blur(8px)" }}
-        ></div>
+        <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent"></div>
         <div className="absolute top-0 z-10 mt-2 flex w-full flex-row items-center justify-between text-sm text-white">
           <span className="flex items-center">
-            <svg
-              className={`h-4 w-4 fill-current text-white`}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 .587l3.668 7.431 8.2 1.192-5.934 5.787 1.4 8.168L12 18.896l-7.334 3.869 1.4-8.168L.132 9.21l8.2-1.192z" />
-            </svg>
-            {/* <span className="ml-1">{project._count.stars}</span> */}
+            <GoStarFill className="text-yellow-400" />
+            <span className="ml-1">{project._count.stars}</span>
           </span>
-
           <Badge variant="secondary" className="mr-6 text-xs">
             {project.status}
           </Badge>
