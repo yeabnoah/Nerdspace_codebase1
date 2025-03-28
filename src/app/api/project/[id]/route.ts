00d5f904941f56f3468db5a114192fb10030ba50
yeabnoah <yeabnoah5@gmail.com>
2025-03-28 13:32:19 +0300

    working on the api to add user Update

--- src/app/api/project/[id]/route.ts
+++ src/app/api/project/[id]/route.ts
@@ -55,3 +55,61 @@ export async function GET(
     );
   }
 }
+
+export const Post = async (
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) => {
+  try {
+    const session = await getUserSession();
+    if (!session) {
+      return NextResponse.json(
+        {
+          message: "unauthorized | not logged in",
+        },
+        { status: 400 },
+      );
+    }
+
+    const { id } = params;
+    const { image, title, content } = await req.json();
+
+    const project = await prisma.project.findFirst({
+      where: {
+        id: id,
+        userId: session.user.id,
+      },
+    });
+
+    if (!project) {
+      return NextResponse.json(
+        {
+          message: "Unauthorized | Project does not belong to the user",
+        },
+        { status: 403 },
+      );
+    }
+
+    const newUpdate = await prisma.projectUpdate.create({
+      data: {
+        projectId: id,
+        title,
+        image,
+        content,
+        userId: session.user.id,
+      },
+    });
+
+    return NextResponse.json({
+      data: newUpdate,
+    });
+  } catch (error) {
+    console.error("Error creating project update:", error);
+    return NextResponse.json(
+      {
+        message: "Internal server error",
+      },
+      { status: 500 },
+    );
+  }
+};
