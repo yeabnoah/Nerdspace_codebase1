c8f5a94dbbe3774dc4b764c1f2c900adddf4ce05
yeabnoah <yeabnoah5@gmail.com>
2025-03-31 15:36:36 +0300

    working: leaderboard

--- src/app/(app)/project/page.tsx
+++ src/app/(app)/project/page.tsx
@@ -1,5 +1,6 @@
 import LeftNavbar from "@/components/navbar/left-navbar";
 import MobileNavBar from "@/components/navbar/mobile-nav-bar";
+import LeaderboardPage from "@/components/project/leaderBorad";
 import ProjectsPage from "@/components/project/project-component";
 import ProjectComponent from "@/components/project/project-component";
 import React from "react";
@@ -8,7 +9,7 @@ const Project = () => {
   return (
     <div className="mx-auto flex max-w-6xl flex-1 flex-row items-start">
       <LeftNavbar />
-      <div className="my-5 min-h-fit flex-1 md:mx-10">
+      <div className="my-5 min-h-fit flex-1 md:mx-10 flex flex-row items-center">
         <ProjectsPage />
       </div>
 
--- src/components/skeleton/project-card.tsx
+++ src/components/skeleton/project-card.tsx
@@ -4,9 +4,9 @@ import { Skeleton } from "../ui/skeleton";
 export function ProjectCardSkeleton() {
   return (
     <Card className="my-2 flex flex-col overflow-hidden rounded-2xl border shadow-none">
-      <CardContent className="relative flex gap-2 h-64 flex-col justify-end p-4">
-        <Skeleton className=" h-40" />
-        <Skeleton className=" h-12" />
+      <CardContent className="relative flex h-64 flex-col justify-end gap-2 p-4">
+        <Skeleton className="h-56" />
+        <Skeleton className="h-12" />
       </CardContent>
     </Card>
   );
