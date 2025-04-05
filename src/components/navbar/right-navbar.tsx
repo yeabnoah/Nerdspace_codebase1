76e0ff4d63f97c3427f3c858fe6a8fc2fadb337c
yeabnoah <yeabnoah5@gmail.com>
2025-04-05 12:53:36 +0300

    project recommendation

--- src/components/navbar/right-navbar.tsx
+++ src/components/navbar/right-navbar.tsx
@@ -1,9 +1,7 @@
+import { authClient } from "@/lib/auth-client";
 import { prisma } from "@/lib/prisma";
 import FollowList from "../user/followList";
-import { authClient } from "@/lib/auth-client";
-import { auth } from "@/lib/auth";
-import { TrendingTopics } from "../user/trending";
-import CommunitiesList from "../user/community";
+import RecomendedProjects from "../user/recommend-project";
 
 const RightNavbar = async () => {
   const user = await authClient.getSession();
@@ -17,7 +15,8 @@ const RightNavbar = async () => {
   return (
     <div className="sticky left-0 top-20 md:pr-5 lg:pr-0">
       <FollowList />
-      <CommunitiesList />
+      <RecomendedProjects />
+      
     </div>
   );
 };
---
+++ src/components/user/recommend-project-page.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import UserInterface from "@/interface/auth/user.interface";
+import { authClient } from "@/lib/auth-client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { Button } from "../ui/button";
+import {
+    ProjectRecommendationSkeleton,
+} from "./FollowListSkeleton";
+
+const ProjectRecommendationList = () => {
+  const [cursor, setCursor] = useState<string | null>(null);
+  const queryClient = useQueryClient();
+  const session = authClient.useSession();
+  const router = useRouter();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["users", cursor],
+    queryFn: async () => {
+      const response = await axios.get(
+        `/api/user/follow/recommendation?curosr=${cursor}`,
+      );
+      return response.data;
+    },
+  });
+
+  const followMutation = useMutation({
+    mutationKey: ["follow-user"],
+    mutationFn: async (userId: string) => {
+      const response = await axios.post(`/api/user/follow?userId=${userId}`);
+      return response.data.message;
+    },
+    onSuccess: (message) => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+      toast.success(message);
+    },
+  });
+
+  const handleFollow = (userId: string) => {
+    if (session.data?.user.id === userId) {
+      toast.error("You cannot follow yourself");
+      return;
+    }
+    followMutation.mutate(userId);
+  };
+
+  if (isLoading) return <ProjectRecommendationSkeleton />;
+  if (isError) return <p>Error loading users</p>;
+
+  const users: UserInterface[] = data?.data || [];
+  const nextCursor: string | null = data?.nextCursor || null;
+
+  return (
+    <div className="my-5 hidden gap-2 rounded-xl border border-gray-100 px-4 py-5 shadow-none dark:border-gray-500/5 md:flex md:flex-col lg:w-[19vw]">
+      <div>
+        <h1
+          className="font-instrument text-2xl italic hover:cursor-pointer hover:underline"
+          onClick={() => {
+            router.push("/whotofollow");
+          }}
+        >
+          Who to Follow
+        </h1>
+        <div className="space-y-3">
+          {users.length > 0 ? (
+            users.map((u) => (
+              <div
+                key={u.id}
+                className="my-5 flex items-center justify-between"
+              >
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={u.image || "/user.jpg"}
+                    alt="user"
+                    className="size-10 rounded-full"
+                    height={200}
+                    width={200}
+                  />
+                  <div className="flex flex-col items-start">
+                    <span className="text-sm">{u.visualName}</span>
+                    <span className="text-[12px]">Nerd@{u.nerdAt}</span>
+                  </div>
+                </div>
+                <Button
+                  size="sm"
+                  className="border bg-transparent text-card-foreground shadow-none hover:bg-transparent dark:text-white"
+                  onClick={() => handleFollow(u.id)}
+                >
+                  Follow
+                </Button>
+              </div>
+            ))
+          ) : (
+            <p className="text-sm text-gray-500">No suggestions available</p>
+          )}
+        </div>
+        {nextCursor && (
+          <Button onClick={() => setCursor(nextCursor)} className="mt-4">
+            Load More
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectRecommendationList;
