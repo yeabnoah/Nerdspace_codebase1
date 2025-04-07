0948d75c01e887e601f3a725ce29f809b608496f
yeabnoah <yeabnoah5@gmail.com>
2025-04-07 18:30:52 +0300

    ui change

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -6,9 +6,9 @@ import RenderPOst from "@/components/post/render-post";
 
 export default function Home() {
   return (
-    <div className="mx-auto flex max-w-6xl flex-1 flex-row items-start">
+    <div className="relative mx-auto flex max-w-6xl flex-1 flex-row items-start">
       <LeftNavbar />
-      <div className="my-5 min-h-fit flex-1 md:mx-10">
+      <div className="my-5 min-h-fit flex-1 md:mx-7">
         {/* <PostInput /> */}
         <RenderPOst />
       </div>
