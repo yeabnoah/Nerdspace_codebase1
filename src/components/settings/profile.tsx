f480e0c4605d343288736d7394db88aa02d876cd
yeabnoah <yeabnoah5@gmail.com>
2025-04-11 03:45:47 +0300

    profile ui polish

--- src/components/settings/profile.tsx
+++ src/components/settings/profile.tsx
@@ -11,6 +11,9 @@ import {
   Lock,
   UsersRound,
   Hammer,
+  ExternalLink,
+  CalendarIcon,
+  Dot,
 } from "lucide-react";
 import useUserStore from "@/store/user.store";
 import RenderMyPost from "./myposts";
@@ -24,6 +27,9 @@ import useUserProfileStore from "@/store/userProfile.store";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Link from "next/link";
+import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { formatDistanceToNow } from "date-fns";
 
 export default function ProfilePage() {
   const [activeTab, setActiveTab] = useState("posts");
@@ -47,164 +53,153 @@ export default function ProfilePage() {
 
   if (isloading || isFetching || isPending) {
     return (
-      <div className="mx-auto flex max-w-7xl flex-1 flex-row items-start justify-center">
-        {/* <LeftNavbar /> */}
-        <div className="mx-auto min-h-screen w-[90%] px-4 sm:px-6 lg:px-8">
-          <div className="relative h-40 overflow-hidden rounded-xl border dark:border-gray-500/5">
-            <Skeleton className="h-full w-full" />
+      <div className="font-geist container relative mx-10 pb-8">
+        <div className="absolute -right-10 -top-20 h-[300px] w-[300px] -rotate-45 rounded-full bg-gradient-to-br from-amber-300/10 to-transparent blur-[80px] dark:from-orange-300/10"></div>
+
+        <div className="group relative mb-12 h-[400px] w-full overflow-hidden rounded-2xl shadow-lg md:h-[200px]">
+          <Skeleton className="h-full w-full" />
+        </div>
+
+        <div className="relative z-10 mx-2 my-5 -mt-16 flex flex-col items-start gap-1">
+          <div className="relative mx-5 -mt-16 h-28 w-28 overflow-hidden rounded-full ring-2 ring-white/20">
+            <Skeleton className="h-full w-full rounded-full" />
           </div>
-          <div className="relative px-6 pb-6">
-            <div className="-mt-16 flex flex-col">
-              <div className="relative">
-                <Skeleton className="size-24 rounded-full" />
-              </div>
-              <Skeleton className="mb-2 mt-2 h-10 w-1/2" />
-              {/* <Skeleton className="h-4 w-1/3" /> */}
-              <Skeleton className="mb-4 h-4 w-3/4" />
-            </div>
-          </div>
-          <div className="px-6 pb-20">
-            <Skeleton className="h-10 w-full" />
-            <div className="mt-4">
-              <Skeleton className="h-64 w-full" />
+          <div className="flex flex-col gap-2">
+            <Skeleton className="h-6 w-48" />
+            <Skeleton className="h-4 w-32" />
+            <div className="flex gap-4">
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="h-4 w-24" />
             </div>
           </div>
         </div>
-        {/* <MobileNavBar /> */}
+
+        <div className="mt-8">
+          <div className="space-y-8 lg:col-span-2">
+            <div className="overflow-hidden rounded-xl border border-gray-100 bg-white shadow-sm dark:border-gray-500/5 dark:bg-black">
+              <div className="p-8">
+                <div className="mb-4">
+                  <Skeleton className="h-10 w-full" />
+                </div>
+                <div className="mt-4">
+                  <Skeleton className="h-64 w-full" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 
   return (
-    <div className="mx-auto min-h-screen w-full px-4 md:w-[70%] md:px-8">
-      <div className="relative h-40 overflow-hidden rounded-xl border bg-transparent dark:border-gray-500/5">
+    <div className="font-geist container relative mx-10 pb-8">
+      <div className="absolute -right-10 -top-20 h-[300px] w-[300px] -rotate-45 rounded-full bg-gradient-to-br from-amber-300/10 to-transparent blur-[80px] dark:from-orange-300/10"></div>
+
+      <div className="group relative mb-12 h-[400px] w-full overflow-hidden rounded-2xl shadow-lg md:h-[200px]">
         <Image
           src={user.coverImage || "/obsession.jpg"}
-          className="w-full bg-cover bg-center"
-          height={800}
-          width={800}
-          alt="test"
+          alt="Cover Image"
+          fill
+          quality={100}
+          className="object-cover"
+          priority={true}
         />
-        <Button
-          onClick={() => {
-            router.push("/settings");
-          }}
-          variant="ghost"
-          size="icon"
-          className="absolute right-4 top-4 bg-background/20 text-white backdrop-blur-sm hover:bg-background/30"
-        >
-          <Settings className="h-5 w-5" />
-        </Button>
-      </div>
-
-      {/* Profile Info Section */}
-      <div className="relative mx-4 pb-6 md:px-6">
-        <div className="-mt-12 flex flex-col items-start md:-mt-16">
-          <div className="relative">
-            <div className="size-20 overflow-hidden rounded-full border md:size-24">
-              <Image
-                src={user.image || "/user.jpg?height=128&width=128"}
-                alt="Emma Smith"
-                width={128}
-                height={128}
-                className="object-cover"
-              />
-            </div>
-          </div>
-
-          <h1 className="mt-2 text-center md:text-left md:text-lg">
-            {user.visualName || user.name}
-          </h1>
-          <p className="text-center text-sm text-muted-foreground md:text-left">
-            Nerd@{user.nerdAt}
-          </p>
-          <p className="mb-2 text-center text-sm text-muted-foreground md:text-left">
-            {user.bio}
-          </p>
-          <div className="mb-4 flex gap-4">
-            <Link
-              href={`/profile/${user.id}/following`}
-              className="text-sm text-muted-foreground hover:text-primary"
-            >
-              <span className="font-medium text-foreground">
-                {user?._count.followers || 0}
-              </span>{" "}
-              Following
-            </Link>
-            <Link
-              href={`/profile/${user.id}/followers`}
-              className="text-sm text-muted-foreground hover:text-primary"
-            >
-              <span className="font-medium text-foreground">
-                {user._count.following || 0}
-              </span>{" "}
-              Followers
-            </Link>
-          </div>
+        <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-zinc-900/90 via-zinc-900/60 to-transparent p-8 dark:from-black/80 dark:via-black/50">
+          <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-zinc-900/90 to-transparent dark:from-black"></div>
         </div>
       </div>
 
-      {/* Content Tabs */}
-      <div className="pb-20 md:px-6">
-        <Tabs
-          defaultValue="posts"
-          className="w-full"
-          onValueChange={setActiveTab}
-        >
-          <TabsList className="scrollbar-hide mb-2 flex justify-start overflow-x-auto bg-transparent">
-            <TabsTrigger
-              value="posts"
-              className="data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
-            >
-              <Grid3X3 className="mr-2 h-4 w-4" />
-              <span className="hidden sm:inline">Posts</span>
-            </TabsTrigger>
-            <TabsTrigger
-              value="projects"
-              className="data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
-            >
-              <Hammer className="mr-2 h-4 w-4" />
-              <span className="hidden sm:inline">Projects</span>
-            </TabsTrigger>
-            {/* <TabsTrigger
-              value="collections"
-              className="data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
-            >
-              <UsersRound className="mr-2 h-4 w-4" />
-              <span className="hidden sm:inline">Community</span>
-            </TabsTrigger> */}
-            <TabsTrigger
-              value="bookmarks"
-              className="data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
-            >
-              <Bookmark className="mr-2 h-4 w-4" />
-              <span className="hidden sm:inline">Bookmarks</span>
-            </TabsTrigger>
-            <TabsTrigger
-              value="private"
-              className="data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
-            >
-              <Lock className="mr-2 h-4 w-4" />
-              <span className="hidden sm:inline">Private</span>
-            </TabsTrigger>
-          </TabsList>
+      <div className="relative z-10 mx-2 my-5 -mt-16 flex flex-col items-start gap-1">
+        <div className="relative mx-5 -mt-16 h-28 w-28 overflow-hidden rounded-full ring-2 ring-white/20">
+          <Image
+            src={user.image || "/user.jpg?height=128&width=128"}
+            alt={user.visualName || user.name}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <h1 className="font-geist mt-2 flex flex-row items-center text-2xl font-medium text-white">
+          <span className="font-geist text-sm font-medium text-white">
+            {user.visualName || user.name}
+          </span>
+          <Dot className="mx-1 h-2 w-2" />
+          <span className="font-geist text-sm font-medium text-white">
+            Nerd@{user.nerdAt}
+          </span>
+        </h1>
 
-          <TabsContent value="posts" className="mt-0">
-            <RenderMyPost />
-          </TabsContent>
-          <TabsContent value="projects" className="mt-0">
-            <ProjectsTab />
-          </TabsContent>
-          <TabsContent value="collections" className="mt-0">
-            <CollectionsTab />
-          </TabsContent>
-          <TabsContent value="bookmarks" className="mt-0">
-            <BookmarksTab />
-          </TabsContent>
-          <TabsContent value="private" className="mt-0">
-            <PrivateTab />
-          </TabsContent>
-        </Tabs>
+        <h1 className="font-geist text-sm font-normal text-white">
+          {user.bio || "No bio"}
+        </h1>
+        <div className="flex gap-4">
+          <span className="font-geist text-sm font-normal text-white">
+            {user._count?.followers || 0} Followers
+          </span>
+          <span className="font-geist text-sm font-normal text-white">
+            {user._count?.following || 0} Following
+          </span>
+        </div>
+      </div>
+      <div className="">
+        <div className="space-y-8 lg:col-span-2">
+          <Card className="overflow-hidden rounded-xl border border-gray-100 bg-white shadow-sm dark:border-gray-500/5 dark:bg-black">
+            <CardContent className="p-8">
+              <Tabs
+                defaultValue="posts"
+                className="w-full"
+                onValueChange={setActiveTab}
+              >
+                <TabsList className="scrollbar-hide mb-2 flex h-12 justify-start overflow-x-auto bg-transparent">
+                  <TabsTrigger
+                    value="posts"
+                    className="h-10 rounded-full data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
+                  >
+                    <Grid3X3 className="mr-2 h-4 w-4" />
+                    <span className="hidden sm:inline">Posts</span>
+                  </TabsTrigger>
+                  <TabsTrigger
+                    value="projects"
+                    className="h-10 rounded-full data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
+                  >
+                    <Hammer className="mr-2 h-4 w-4" />
+                    <span className="hidden sm:inline">Projects</span>
+                  </TabsTrigger>
+                  <TabsTrigger
+                    value="bookmarks"
+                    className="h-10 rounded-full data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
+                  >
+                    <Bookmark className="mr-2 h-4 w-4" />
+                    <span className="hidden sm:inline">Bookmarks</span>
+                  </TabsTrigger>
+                  <TabsTrigger
+                    value="private"
+                    className="h-10 rounded-full data-[state=active]:bg-primary data-[state=active]:text-primary-foreground"
+                  >
+                    <Lock className="mr-2 h-4 w-4" />
+                    <span className="hidden sm:inline">Private</span>
+                  </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="posts" className="mt-0">
+                  <RenderMyPost />
+                </TabsContent>
+                <TabsContent value="projects" className="mt-0">
+                  <ProjectsTab />
+                </TabsContent>
+                <TabsContent value="collections" className="mt-0">
+                  <CollectionsTab />
+                </TabsContent>
+                <TabsContent value="bookmarks" className="mt-0">
+                  <BookmarksTab />
+                </TabsContent>
+                <TabsContent value="private" className="mt-0">
+                  <PrivateTab />
+                </TabsContent>
+              </Tabs>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   );
