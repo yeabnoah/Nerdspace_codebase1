8e71213f71f8f15cf58ee5e4e8c0a68ec2c1f3bb
yeabnoah <yeabnoah5@gmail.com>
2025-04-11 13:56:38 +0300

    ui : polished the search related stuff

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,43 +1,62 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Check, Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { Check, Laptop, Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-    const { setTheme , theme} = useTheme()
+  const { setTheme, theme } = useTheme();
 
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon" className="dark:bg-textAlternative mx-auto bg-white outline-transparent outline-none after:outline-none">
-                    <Sun className=" size-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute size-[1rem]  rotate-90  scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className=" dark:bg-textAlternative ">
-                <DropdownMenuItem onClick={() => setTheme("light")} className=" flex items-center justify-between">
-                    Light
-                    {theme === "light" && <Check className=" bg-textAlternative/20 p-1 dark:bg-white/60 dark:text-black rounded-full" />}
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")} className=" flex items-center justify-between">
-                    Dark
-                    {theme === "dark" && <Check className=" bg-textAlternative/20 p-1 dark:bg-white/60 rounded-full dark:text-black" />}
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")} className=" flex items-center justify-between ">
-                    System
-                    {theme === "system" && <Check className=" bg-textAlternative/20 p-1 dark:bg-white/60 rounded-full dark:text-black" />}
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="mx-auto bg-white outline-none outline-transparent after:outline-none dark:bg-textAlternative"
+        >
+          <Sun className="absolute size-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute size-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        className="w-48 rounded-2xl border-none bg-white/80 shadow-lg backdrop-blur-sm dark:bg-black/80"
+      >
+        <DropdownMenuItem
+          onClick={() => setTheme("light")}
+          className="h-10 cursor-pointer rounded-xl hover:bg-black/70"
+        >
+          <Sun className="mr-2 h-4 w-4" />
+          <span>Light</span>
+          {theme === "light" && <Check className="ml-auto h-4 w-4" />}
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => setTheme("dark")}
+          className="h-10 cursor-pointer rounded-xl hover:bg-black/70"
+        >
+          <Moon className="mr-2 h-4 w-4" />
+          <span>Dark</span>
+          {theme === "dark" && <Check className="ml-auto h-4 w-4" />}
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => setTheme("system")}
+          className="h-10 cursor-pointer rounded-xl hover:bg-black/70"
+        >
+          <Laptop className="mr-2 h-4 w-4" />
+          <span>System</span>
+          {theme === "system" && <Check className="ml-auto h-4 w-4" />}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
