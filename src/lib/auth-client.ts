46551801316b8672de02a46186ea7f376584b329
yeabnoah <yeabnoah5@gmail.com>
2025-04-15 01:06:24 +0300

    prod: fixing build issues : 7th

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   // baseURL: "http://localhost:3000", // the base url of your auth server
-  baseURL: "https://nerdspacer.com",
+  // baseURL: "https://nerdspacer.com",
+  baseURL: "https://nerd-space-kappa.vercel.app/",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
