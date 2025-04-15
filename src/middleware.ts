8276cbfc83f3062d581967a164584a5382a63c8a
yeabnoah <yeabnoah5@gmail.com>
2025-04-15 10:53:06 +0300

    middleware-check: fixing the auth issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,54 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth";
+// import { NextRequest, NextResponse } from "next/server";
+// import { getSessionCookie } from "better-auth";
 
+// export async function middleware(request: NextRequest) {
+//   const sessionCookie = getSessionCookie(request);
+//   if (!sessionCookie) {
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
+//   return NextResponse.next();
+// }
+
+// // const
+// export const config = {
+//   runtime: "experimental-edge",
+//   matcher: ["/dashboard"],
+// };
+
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
-  if (!sessionCookie) {
+  const { pathname } = request.nextUrl;
+  if (
+    sessionCookie &&
+    ["/login", "/signup", "/reset-password", "/forgot-password"].includes(
+      pathname,
+    )
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+  if (!sessionCookie && pathname.startsWith("/")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
   return NextResponse.next();
 }
 
-// const
 export const config = {
-  runtime: "experimental-edge",
-  matcher: ["/dashboard"],
+  matcher: [
+    "/",
+    "/profile",
+    "/settings",
+    "/projects",
+    "/onboarding",
+    "/pos",
+    "/ai",
+    "/community",
+    "/user-profile",
+    "/sandbox",
+    "/whotofollow",
+    "/explore",
+    "/event",
+    "/project",
+  ],
 };
