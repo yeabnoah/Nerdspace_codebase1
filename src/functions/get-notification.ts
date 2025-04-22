19bb5e9b4f7d0536b1f6ddc8578bc97b95c62628
yeabnoah <yeabnoah5@gmail.com>
2025-04-22 09:10:26 +0300

    .env.example

---
+++ src/functions/get-notification.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import getUserSession from "./get-user";
+
+const getNotifications = async () => {
+  const session = await getUserSession();
+
+  if (!session) {
+    return null;
+  }
+
+  const notifications = await axios.get("/api/notification");
+
+  return notifications.data;
+};
+
+export default getNotifications;
