2095fce8f3d01a451b2aee3acd40afdceaddff91
yeabnoah <yeabnoah5@gmail.com>
2025-04-24 01:16:25 +0300

    [userid]/projects/route.ts export get fixed

--- src/app/api/users/[userId]/projects/route.ts
+++ src/app/api/users/[userId]/projects/route.ts
@@ -2,10 +2,10 @@ import getUserSession from "@/functions/get-user";
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (
+export async function GET(
   request: NextRequest,
   { params }: { params: { userId: string } },
-) => {
+) {
   try {
     const session = await getUserSession();
     if (!session) {
@@ -88,4 +88,4 @@ export const GET = async (
       { status: 500 },
     );
   }
-};
+}
